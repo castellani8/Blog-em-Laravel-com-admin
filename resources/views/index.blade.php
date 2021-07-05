8d6b34902356d2823ffa5d223bd7fb4c46ca1943
Lucas Castellani <lucas100castellani@gmail.com>
2021-07-05 01:25:31 -0300

    estruturacao da index 30%

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,41 @@
 
 @section('content')
 
-<div class="container"> Lucas </div>
+    <img class="img-fluid" src="{{ asset('frontend/images/capa.jpg') }}" style="width:100%;height:90vh">
+    <div class="container">
+        <div class="row my-3 justify-content-center text-center">
+            <h1>A igreja de Cristo</h1>
+            <h5>Somos uma igreja com o intuito de fazer . What is Lorem Ipsum?
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum   </h5>
+        </div>
+    </div>
+
+    {{-- sessao de projetos --}}
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 bg-dark">
+                <div class="p-5"></div>
+            </div>
+            <div class="col-md-4 bg-danger">
+                <div class="p-5"></div>
+            </div>
+            <div class="col-md-4 bg-warning">
+                <div class="p-5"></div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 bg-primary">
+                <div class="p-5"></div>
+            </div>
+            <div class="col-md-4 bg-secondary">
+                <div class="p-5"></div>
+            </div>
+            <div class="col-md-4 bg-dark">
+                <div class="p-5"></div>
+            </div>
+        </div>
+    </div>
 
 @endsection
--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -8,9 +8,45 @@
     <title>Document</title>
 </head>
 <body>
+    <nav class='navbar navbar-expand-lg navbar-dark bg-dark navcolor p-1'>
+        <div class='container-fluid justify-content-center'>
+            <button class='mr-auto navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+                <span class='navbar-toggler-icon'></span>
+            </button>
+            <a class='navbar-brand mr-auto text-light' href='#'><img class=' p-0 mr-auto'style='width:30%'>O novo mundo de cristo</a>
+            <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul class='navbar-nav ml-auto mb-2 mb-lg-0 text-center'>
+                <li class='nav-item'>
+                <a class='nav-link text-light active' aria-current='page' href='index'>Home</a>
+                </li>
+                <div class='divider-vertical d-lg-block d-none my-auto'></div>
+                <li class='nav-item'>
+                <a class='nav-link text-light' href='QuemSomos.php'>Quem Somos</a>
+                </li>
+                <div class='divider-vertical d-lg-block d-none my-auto'></div>
+                <li class='nav-item'>
+                <a class='nav-link text-light' href='Projetos.php'>Projetos</a>
+                </li>
+                <div class='divider-vertical d-lg-block d-none my-auto'></div>
+                <li class='nav-item'>
+                <a class='nav-link text-light' href='Paineis.php'>Painéis</a>
+                </li>
+                <div class='divider-vertical d-lg-block d-none my-auto'></div>
+                <li class='nav-item'>
+                <a class='nav-link text-light' href='FaleConosco.php'>Fale Conosco</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
     <div>
         @yield('content')
     </div>    
     <script src="{{ asset('js/app.js') }}"></script>    
+    <style>
+        .text-light{
+            color:black;
+        }
+    </style>
 </body>
 </html>
